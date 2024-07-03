627fb50ac819a8bdc4e30a73e103581649e636a8
jahnavisai03 <jahnavisai2002@gmail.com>
2024-07-03 14:47:33 +0530

    RAG in aws usin docker

---
+++ User/app.py
@@ -0,0 +1,109 @@
+import boto3
+import streamlit as st
+import os
+import uuid
+import logging
+
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+## s3_client
+s3_client = boto3.client("s3")
+BUCKET_NAME = os.getenv("BUCKET_NAME")
+
+## Bedrock
+from langchain_community.embeddings import BedrockEmbeddings
+from langchain.llms.bedrock import Bedrock
+
+## prompt and chain
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+
+## Text Splitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+
+
+## import FAISS
+from langchain_community.vectorstores import FAISS
+
+bedrock_client = boto3.client(service_name="bedrock-runtime",region_name="us-east-1")
+bedrock_embeddings = BedrockEmbeddings(model_id="amazon.titan-embed-text-v1", client=bedrock_client)
+
+folder_path = "/tmp/"
+
+def get_unique_id():
+    return str(uuid.uuid4())
+
+def load_index():
+    s3_client.download_file(Bucket=BUCKET_NAME,Key="my_faiss.faiss",Filename=f"{folder_path}my_faiss.faiss")
+    s3_client.download_file(Bucket=BUCKET_NAME,Key="my_faiss.pkl",Filename=f"{folder_path}my_faiss.pkl")
+    
+def get_llm():
+    llm=Bedrock(model_id="meta.llama3-70b-instruct-v1:0", client=bedrock_client,
+                )
+    return llm
+    
+def get_response(llm,vectorstore, question ):
+    ## create prompt / template
+    prompt_template = """
+
+    Human: Please use the given context to provide concise answer to the question
+    If you don't know the answer, just say that you don't know, don't try to make up an answer and try to provide relevant references at the end
+    <context>
+    {context}
+    </context>
+
+    Question: {question}
+
+    Assistant:"""
+
+    PROMPT = PromptTemplate(
+        template=prompt_template, input_variables=["context", "question"]
+    )
+
+    qa = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=vectorstore.as_retriever(
+        search_type="similarity", search_kwargs={"k": 5}
+    ),
+    return_source_documents=True,
+    chain_type_kwargs={"prompt": PROMPT}
+)
+    answer=qa({"query":question})
+    return answer['result']
+
+## main method
+def main():
+    st.header("This is Client Site for Chat with PDF demo")
+    load_index()
+    
+    dir_list = os.listdir(folder_path)
+    st.write(f"Files and Directories in {folder_path}")
+    st.write(dir_list)
+    ## create index
+    faiss_index = FAISS.load_local(
+        index_name="my_faiss",
+        folder_path = folder_path,
+        embeddings=bedrock_embeddings,
+        allow_dangerous_deserialization=True
+    )
+    st.write("INDEX IS READY")
+    question = st.text_input("Please ask your question")
+    if st.button("Ask Question"):
+        with st.spinner("Querying..."):
+
+            llm = get_llm()
+
+            # get_response
+            st.write(get_response(llm, faiss_index, question))
+            st.success("Done")
+
+
+
+
+if __name__ == "__main__":
+    main()
